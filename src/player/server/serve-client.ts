c683ccc054e7d15c452113e1e03824698d7ca766
olyop <oliver.plummer@outlook.com>
2021-10-10 19:15:03 +1100

    Update

--- src/player/server/serve-client.ts
+++ src/player/server/serve-client.ts
@@ -5,7 +5,7 @@ import { CLIENT_ENTRY_PATH } from "./globals"
 const serveClient: FastifyPluginCallback =
 	(fastify, options, next) => {
 		fastify.setNotFoundHandler((request, reply) => {
-			reply.status(200).sendFile(CLIENT_ENTRY_PATH)
+			reply.type("text/html").sendFile(CLIENT_ENTRY_PATH)
 		})
 		next()
 	}
