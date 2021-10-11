74fd58cbbd4a82dd771493d1f2fa7512b58766e8
olyop <oliver.plummer@outlook.com>
2021-10-11 15:41:09 +1100

    Update

--- src/player/client/hooks/is-pwa.ts
+++ src/player/client/hooks/is-pwa.ts
@@ -1,11 +1,11 @@
-declare global {
-	interface Navigator {
-		standalone: boolean,
-	}
-}
+// declare global {
+// 	interface Navigator {
+// 		standalone: boolean,
+// 	}
+// }
 
 export const useIsPWA =
 	() =>
-		document.referrer.includes("android-app://") ||
-		navigator.standalone === true ||
-		matchMedia("(display-mode: standalone)").matches
+		document.referrer.includes("android-app://")
+		// navigator.standalone === true ||
+		// matchMedia("(display-mode: standalone)").matches
