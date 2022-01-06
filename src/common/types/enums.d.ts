b7656b8abc23a20ed27786dbf756ed218928e822
olyop <oliver.plummer@outlook.com>
2022-01-06 13:50:02 +1100

    Update

--- src/common/types/enums.d.ts
+++
@@ -1,9 +0,0 @@
-export declare enum ImageSizes {
-    MINI = "mini",
-    HALF = "half",
-    FULL = "full"
-}
-export declare enum ImageDimensions {
-    SQUARE = "square",
-    LANDSCAPE = "landscape"
-}
