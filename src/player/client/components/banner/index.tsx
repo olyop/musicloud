d557abc3e747dc7b7d2654707135495082013c38
olyop <oliver.plummer@outlook.com>
2022-01-27 11:25:14 +1100

    Update

--- src/player/client/components/banner/index.tsx
+++ src/player/client/components/banner/index.tsx
@@ -2,10 +2,6 @@ import Image from "@oly_op/react-image"
 import { createBEM } from "@oly_op/bem"
 import { createElement, VFC, ReactNode } from "react"
 
-import "@oly_op/css-utilities/index.css"
-import "@oly_op/react-button/build/index.css"
-import "@oly_op/react-image/build/index.css"
-
 import "./index.scss"
 
 const bem =
