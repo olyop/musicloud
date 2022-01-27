36e012a93909300d98326882ce5be635f762fe72
olyop <oliver.plummer@outlook.com>
2022-01-27 12:28:49 +1100

    Update

--- src/player/client/service-worker.ts
+++ src/player/client/service-worker.ts
@@ -27,28 +27,28 @@ setDefaultHandler(
 
 offlineFallback()
 
-registerRoute(
-	({ url }) =>
-		url.origin === "https://fonts.googleapis.com",
-	new StaleWhileRevalidate({
-		cacheName: "google-fonts-stylesheets",
-	}),
-)
+// registerRoute(
+// 	({ url }) =>
+// 		url.origin === "https://fonts.googleapis.com",
+// 	new StaleWhileRevalidate({
+// 		cacheName: "google-fonts-stylesheets",
+// 	}),
+// )
 
-registerRoute(
-	({ url }) =>
-		url.origin === "https://fonts.gstatic.com",
-	new CacheFirst({
-		cacheName: "google-fonts-webfonts",
-		plugins: [
-			new CacheableResponsePlugin({ statuses: [200] }),
-			new ExpirationPlugin({
-				maxAgeSeconds: 60 * 60 * 24 * 365,
-				maxEntries: 30,
-			}),
-		],
-	}),
-)
+// registerRoute(
+// 	({ url }) =>
+// 		url.origin === "https://fonts.gstatic.com",
+// 	new CacheFirst({
+// 		cacheName: "google-fonts-webfonts",
+// 		plugins: [
+// 			new CacheableResponsePlugin({ statuses: [200] }),
+// 			new ExpirationPlugin({
+// 				maxAgeSeconds: 60 * 60 * 24 * 365,
+// 				maxEntries: 30,
+// 			}),
+// 		],
+// 	}),
+// )
 
 registerRoute(
 	({ request }) =>
