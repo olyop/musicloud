f74a1aacf72089037f728e33dbb4e686d81bc376
olyop <oliver.plummer@outlook.com>
2022-02-01 12:08:11 +1100

    Update

--- src/common/src/options.ts
+++ src/common/src/options.ts
@@ -1,11 +1,6 @@
 import { PoolConfig } from "pg"
 import { fastifyHelmet } from "fastify-helmet"
-import { FastifyServerOptions } from "fastify"
 import { FastifyCorsOptions } from "fastify-cors"
-
-export const FASTIFY_SERVER_OPTIONS: FastifyServerOptions = {
-	logger: process.env.NODE_ENV === "production",
-}
 
 export const PG_POOL_OPTIONS: PoolConfig = {
 	parseInputDatesAsUTC: true,
